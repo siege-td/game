413df154cac5f238c89698b71f703854f0729fa1
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-03-08 13:49:07 +0100

    Update gitignore

---
+++ core/src/com/siegetd/game/models/ecs/systems/MovementSystem.java
@@ -0,0 +1,10 @@
+package com.siegetd.game.models.ecs.systems;
+
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.siegetd.game.models.ecs.components.TransformComponent;
+
+public class MovementSystem extends IteratingSystem {
+    private ComponentMapper<TransformComponent> transformMapper;
+    private 
+}
