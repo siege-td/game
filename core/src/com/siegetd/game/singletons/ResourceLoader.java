5400996ee8667f30d5d6c49b900038392ac7257e
Eskil Torland <eskil.torland@gmail.com>
2022-03-08 13:58:35 +0100

    Early Implementation of the singleton ResourceLoader

--- core/src/com/siegetd/game/singletons/ResourceLoader.java
+++ core/src/com/siegetd/game/singletons/ResourceLoader.java
@@ -1,4 +1,62 @@
 package com.siegetd.game.singletons;
 
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import java.util.HashMap;
+
 public class ResourceLoader {
+    public static ResourceLoader resourceLoaderInstance = null;
+
+    private HashMap<String, Sound> sounds;
+    private HashMap<String, Music> music;
+
+    private HashMap<String, TextureRegion> attackers;
+    private HashMap<String, TextureRegion> defenders;
+
+    private HashMap<String, TextureRegion> maps;
+    private HashMap<String, TextureRegion> projectiles;
+    private HashMap<String, TextureRegion> collisions;
+
+    public ResourceLoader(){
+
+    }
+    public static ResourceLoader getInstance(){
+        if(resourceLoaderInstance == null){
+            resourceLoaderInstance = new ResourceLoader();
+        }
+        return resourceLoaderInstance;
+    }
+
+    private Sound getSoundById(String id){
+        return sounds.get(id);
+    }
+
+    private Music getMusicById(String id){
+        return music.get(id);
+    }
+
+    private TextureRegion getAttackerTexture(String id){
+        return attackers.get(id);
+    }
+
+    private TextureRegion getDefenderTexture(String id){
+        return defenders.get(id);
+    }
+
+    private TextureRegion getMapById(String id){
+        return maps.get(id);
+    }
+
+    private TextureRegion getProjectileTexture(String id){
+        return projectiles.get(id);
+    }
+
+    private TextureRegion getCollisionTexture(String id){
+        return collisions.get(id);
+    }
+/**TODO**/
+//Instantiate Files
+
 }
