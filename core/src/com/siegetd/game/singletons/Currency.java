691672c9e8b526c4601d46b671090c001d152dad
Eskil Torland <eskil.torland@gmail.com>
2022-03-08 11:38:54 +0100

    First iteration of currency singleton

--- core/src/com/siegetd/game/singletons/Currency.java
+++ core/src/com/siegetd/game/singletons/Currency.java
@@ -1,4 +1,29 @@
 package com.siegetd.game.singletons;
 
 public class Currency {
+    private static Currency currencyInstance = null;
+
+    private int currency;
+
+    public Currency(){
+        this.currency = 100;
+    }
+    private void addCurrency(int amount){
+        currency += amount;
+    }
+
+    public static Currency getInstance(){
+        if(currencyInstance == null){
+            currencyInstance = new Currency();
+        }
+        return currencyInstance;
+    }
+
+    private void subtractCurrency(int amount){
+        currency -= amount;
+    }
+
+    public int getCurrency() {
+        return currency;
+    }
 }
