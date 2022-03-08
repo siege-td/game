e4e44570eb4a63c6c15bae7de7fbd06bc0786f28
Eskil Torland <eskil.torland@gmail.com>
2022-03-08 13:13:21 +0100

    Early incomplete iteration of scorehandling'

--- core/src/com/siegetd/game/singletons/ScoreHandler.java
+++ core/src/com/siegetd/game/singletons/ScoreHandler.java
@@ -1,4 +1,31 @@
 package com.siegetd.game.singletons;
 
+import java.util.HashMap;
+
 public class ScoreHandler {
+    private static ScoreHandler scoreInstance = null;
+    private HashMap<String,Integer> playerScores;
+
+    public ScoreHandler(){
+    }
+
+    public static ScoreHandler getInstance(){
+        if(scoreInstance == null){
+            scoreInstance = new ScoreHandler();
+        }
+        return scoreInstance;
+    }
+
+    private HashMap<String, Integer> getPlayerScores() {
+        return playerScores;
+    }
+
+    private int getScoreByID(String playerId){
+        return playerScores.get(playerId);
+    }
+
+    private void subtractScore(String playerId, int amount){
+        playerScores.put(playerId,playerScores.get(playerId) - amount);
+    }
+
 }
