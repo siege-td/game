b8c6bbb8f7879570e3cb0d18ae3abce6cb7841ab
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-03-08 14:07:30 +0100

    Add movement controller

--- core/src/com/siegetd/game/models/ecs/systems/MovementSystem.java
+++ core/src/com/siegetd/game/models/ecs/systems/MovementSystem.java
@@ -1,10 +1,29 @@
 package com.siegetd.game.models.ecs.systems;
 
 import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.siegetd.game.models.ecs.components.TransformComponent;
+import com.siegetd.game.models.ecs.components.VelocityComponent;
 
 public class MovementSystem extends IteratingSystem {
     private ComponentMapper<TransformComponent> transformMapper;
-    private 
+    private ComponentMapper<VelocityComponent> velocityMapper;
+
+    public MovementSystem() {
+        super(Family.all(TransformComponent.class, VelocityComponent.class).get());
+
+        transformMapper = ComponentMapper.getFor(TransformComponent.class);
+        velocityMapper = ComponentMapper.getFor(VelocityComponent.class);
+    }
+
+    @Override
+    public void processEntity(Entity entity, float deltaTime) {
+        TransformComponent transformComponent = transformMapper.get(entity);
+        VelocityComponent velocityComponent = velocityMapper.get(entity);
+
+        transformComponent.position.x += velocityComponent.xSpeed * deltaTime;
+        transformComponent.position.y += velocityComponent.ySpeed * deltaTime;
+    }
 }
