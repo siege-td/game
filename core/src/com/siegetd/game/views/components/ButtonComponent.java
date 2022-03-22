042d94e88945753c97a2903f8ed103a30e454331
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-03-22 10:34:33 +0100

    Refactor all the things

--- core/src/com/siegetd/game/views/components/ButtonComponent.java
+++ core/src/com/siegetd/game/views/components/ButtonComponent.java
@@ -1,18 +1,33 @@
 package com.siegetd.game.views.components;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 //TODO: Base the Y-coordinates and size(?) to table instead of graphics
-public abstract class ButtonComponent {
-    protected int btnWidth, btnHeight, btnX, btnY;
-    protected Texture img;
-    public Button btn;
+public class ButtonComponent {
 
+    protected ButtonComponent(){ }
 
-    protected ButtonComponent(){
+    public Button createButton(float yScale, Texture img) {
+        Button button = new Button(
+                new TextureRegionDrawable(
+                        new TextureRegion(img)
+                )
+        );
 
+        button.setSize(button.getWidth() / 2, button.getHeight() / 2);
+        button.setPosition(
+                (Gdx.graphics.getWidth() / 2) - (button.getWidth() / 2),
+                (Gdx.graphics.getHeight() * yScale) - (button.getHeight() / 2)
+                );
+
+        return button;
     }
-    public abstract void dispose();
 
+    public void dispose(Texture buttonImg) {
+        buttonImg.dispose();
+    }
 }
--- core/src/com/siegetd/game/views/components/ButtonHost.java
+++ core/src/com/siegetd/game/views/components/ButtonHost.java
@@ -1,39 +1,41 @@
 package com.siegetd.game.views.components;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.siegetd.game.controllers.GameStateController;
 
-public class ButtonHost extends ButtonComponent {
+public class ButtonHost {
+
+    private ButtonComponent buttonComponent;
+    private Button button;
+    private Texture buttonImg;
+
     public ButtonHost(){
-        img = new Texture("GUI/create_game.png");
-        btn = new Button(new TextureRegionDrawable(new TextureRegion(img)));
-        btnWidth = (int)(btn.getWidth()/2);
-        btnHeight = (int)(btn.getHeight()/2);
-        btnX = ((Gdx.graphics.getWidth() / 2) - (btnWidth / 2));
-        btnY = ((int)(Gdx.graphics.getHeight() * 0.5) - (btnHeight / 2));
-        btn.setPosition(btnX, btnY);
-        btn.setSize(btnWidth, btnHeight);
+//        img = new Texture("GUI/create_game.png");
+//        btn = new Button(new TextureRegionDrawable(new TextureRegion(img)));
+//        btnWidth = (int)(btn.getWidth()/2);
+//        btnHeight = (int)(btn.getHeight()/2);
+//        btnX = ((Gdx.graphics.getWidth() / 2) - (btnWidth / 2));
+//        btnY = ((int)(Gdx.graphics.getHeight() * 0.5) - (btnHeight / 2));
+//        btn.setPosition(btnX, btnY);
+//        btn.setSize(btnWidth, btnHeight);
+
+        this.buttonComponent = new ButtonComponent();
+        this.buttonImg = new Texture("GUI/create_game.png");
+        this.button = this.buttonComponent.createButton(0.5f, this.buttonImg);
     }
 
     public void addButtonListners(final GameStateController gsc) {
         System.out.println("gsc: " + gsc);
-        this.btn.addListener(new ClickListener() {
+        this.button.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 gsc.setState(GameStateController.State.LOBBY);
-                dispose();
+                buttonComponent.dispose(buttonImg);
             }
         });
-    }
-    @Override
-    public void dispose(){
-        img.dispose();
     }
 
 }
