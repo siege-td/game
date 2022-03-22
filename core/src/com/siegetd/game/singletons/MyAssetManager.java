547e9e2d9289e63dc7b77379092b33dc874c21a4
Eskil Torland <eskil.torland@gmail.com>
2022-03-22 13:42:11 +0100

    Removed ResourceLoader and added AssetManager

---
+++ core/src/com/siegetd/game/singletons/MyAssetManager.java
@@ -0,0 +1,32 @@
+package com.siegetd.game.singletons;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.utils.Array;
+
+import java.util.List;
+
+public class MyAssetManager{
+
+    public static MyAssetManager assetManagerInstance = null;
+    private AssetManager assMan;
+
+    private MyAssetManager(){
+        assMan = new AssetManager();
+    }
+
+    public static MyAssetManager getInstance(){
+        if(assetManagerInstance == null){
+            assetManagerInstance = new MyAssetManager();
+        }
+        return assetManagerInstance;
+    }
+
+    private void queAssetsFromList(List<String> paths,Class<?> type ){
+        for (String path:paths) {
+            assMan.load(path, type);
+        }
+    }
+
+    private void queAsset(String path, Class<?> type){
+        assMan.load(path,type);
+    }
+}
