c60eee9da9425497628521fd31c5c1a57a7b2917
Sigurd <sigurd1994@hotmail.com>
2022-04-05 15:21:40 +0200

    font-test

--- core/src/com/siegetd/game/views/states/JoinGameState.java
+++ core/src/com/siegetd/game/views/states/JoinGameState.java
@@ -1,9 +1,11 @@
 package com.siegetd.game.views.states;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.siegetd.game.controllers.GameStateController;
@@ -46,7 +48,15 @@ public class JoinGameState extends GameState {
         inputButton = new InputButton();
         inputButton.addButtonListners(gsc);
         font = new BitmapFont();
-        font.getData().setScale(3f);
+        fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/DimboRegular.ttf"));
+        fontParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+
+        fontParameter.size = 12;
+        fontParameter.color = Color.BLACK;
+
+        font = fontGenerator.generateFont(fontParameter);
+
+        fontGenerator.dispose();
 
         //Stage components
         buttonTable.add(inputButton.button).size(
