133e309f4613047234a744e80a538acc9c76685a
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-04-14 16:33:05 +0200

    Copy Pasta

--- core/src/com/siegetd/game/models/map/tile/MovableTile.java
+++ core/src/com/siegetd/game/models/map/tile/MovableTile.java
@@ -1,4 +1,9 @@
 package com.siegetd.game.models.map.tile;
 
-public class MovableTile {
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+
+public class MovableTile extends Tile {
+    public MovableTile(int x, int y, TiledMapTileLayer.Cell cell){
+        super(x, y, cell);
+    }
 }
--- core/src/com/siegetd/game/models/map/tile/PlaceableTile.java
+++ core/src/com/siegetd/game/models/map/tile/PlaceableTile.java
@@ -1,4 +1,9 @@
 package com.siegetd.game.models.map.tile;
 
-public class PlaceableTile {
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+
+public class PlaceableTile extends Tile {
+    public PlaceableTile(int x, int y, TiledMapTileLayer.Cell cell){
+        super(x, y, cell);
+    }
 }
