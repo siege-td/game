a3e408ebffcf128ec5f193ba2b119b3fbfcd59be
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-04-25 14:06:30 +0200

    Update readme

---
+++ desktop/bin/main/level1/Jungle.tsx
@@ -0,0 +1,133 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Jungle" tilewidth="256" tileheight="256" tilecount="42" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="256" height="256" source="tiles/32.png"/>
+ </tile>
+ <tile id="1">
+  <image width="256" height="256" source="tiles/33.png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="256" source="tiles/34.png"/>
+ </tile>
+ <tile id="3">
+  <image width="256" height="256" source="tiles/35.png"/>
+ </tile>
+ <tile id="4">
+  <image width="256" height="256" source="tiles/36.png"/>
+ </tile>
+ <tile id="5">
+  <image width="256" height="256" source="tiles/37.png"/>
+ </tile>
+ <tile id="6">
+  <image width="256" height="256" source="tiles/38.png"/>
+ </tile>
+ <tile id="7">
+  <image width="256" height="256" source="tiles/39.png"/>
+ </tile>
+ <tile id="8">
+  <image width="256" height="256" source="tiles/40.png"/>
+ </tile>
+ <tile id="9">
+  <image width="256" height="256" source="tiles/41.png"/>
+ </tile>
+ <tile id="10">
+  <image width="256" height="256" source="tiles/42.png"/>
+ </tile>
+ <tile id="11">
+  <image width="256" height="256" source="tiles/1.png"/>
+ </tile>
+ <tile id="12">
+  <image width="256" height="256" source="tiles/2.png"/>
+ </tile>
+ <tile id="13">
+  <image width="256" height="256" source="tiles/3.png"/>
+ </tile>
+ <tile id="14">
+  <image width="256" height="256" source="tiles/4.png"/>
+ </tile>
+ <tile id="15">
+  <image width="256" height="256" source="tiles/5.png"/>
+ </tile>
+ <tile id="16">
+  <image width="256" height="256" source="tiles/6.png"/>
+ </tile>
+ <tile id="17">
+  <image width="256" height="256" source="tiles/7.png"/>
+ </tile>
+ <tile id="18">
+  <image width="256" height="256" source="tiles/8.png"/>
+ </tile>
+ <tile id="19">
+  <image width="256" height="256" source="tiles/9.png"/>
+ </tile>
+ <tile id="20">
+  <image width="256" height="256" source="tiles/10.png"/>
+ </tile>
+ <tile id="21">
+  <image width="256" height="256" source="tiles/11.png"/>
+ </tile>
+ <tile id="22">
+  <image width="256" height="256" source="tiles/12.png"/>
+ </tile>
+ <tile id="23">
+  <image width="256" height="256" source="tiles/13.png"/>
+ </tile>
+ <tile id="24">
+  <image width="256" height="256" source="tiles/14.png"/>
+ </tile>
+ <tile id="25">
+  <image width="256" height="256" source="tiles/15.png"/>
+ </tile>
+ <tile id="26">
+  <image width="256" height="256" source="tiles/16.png"/>
+ </tile>
+ <tile id="27">
+  <image width="256" height="256" source="tiles/17.png"/>
+ </tile>
+ <tile id="28">
+  <image width="256" height="256" source="tiles/18.png"/>
+ </tile>
+ <tile id="29">
+  <image width="256" height="256" source="tiles/19.png"/>
+ </tile>
+ <tile id="30">
+  <image width="256" height="256" source="tiles/20.png"/>
+ </tile>
+ <tile id="31">
+  <image width="256" height="256" source="tiles/21.png"/>
+ </tile>
+ <tile id="32">
+  <image width="256" height="256" source="tiles/22.png"/>
+ </tile>
+ <tile id="33">
+  <image width="256" height="256" source="tiles/23.png"/>
+ </tile>
+ <tile id="34">
+  <image width="256" height="256" source="tiles/24.png"/>
+ </tile>
+ <tile id="35">
+  <image width="256" height="256" source="tiles/25.png"/>
+ </tile>
+ <tile id="36">
+  <image width="256" height="256" source="tiles/26.png"/>
+ </tile>
+ <tile id="37">
+  <image width="256" height="256" source="tiles/27.png"/>
+ </tile>
+ <tile id="38">
+  <image width="256" height="256" source="tiles/28.png"/>
+ </tile>
+ <tile id="39">
+  <image width="256" height="256" source="tiles/29.png"/>
+ </tile>
+ <tile id="40">
+  <image width="256" height="256" source="tiles/30.png"/>
+ </tile>
+ <tile id="41">
+  <image width="256" height="256" source="tiles/31.png"/>
+ </tile>
+  <tile id="42">
+   <image width="256" height="256" source="tiles/43.png"/>
+  </tile>
+</tileset>
